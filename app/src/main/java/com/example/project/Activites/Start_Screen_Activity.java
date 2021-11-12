63af3e7c694648db3b21d961eab7eace160396af
AdemKhemiri <adamkhemiri2222@gmail.com>
2021-11-12 11:23:30 +0100

    SignIn with google

--- app/src/main/java/com/example/project/Activites/Start_Screen_Activity.java
+++ app/src/main/java/com/example/project/Activites/Start_Screen_Activity.java
@@ -1,23 +1,70 @@
 package com.example.project.Activites;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.project.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class Start_Screen_Activity extends AppCompatActivity {
-   private Handler handler = new Handler();
+    public static final int GOOGLE_SIGN_IN_CODE = 10005;
+    private Handler handler = new Handler();
+
+    SignInButton signIn;
+    GoogleSignInOptions gso;
+    GoogleSignInClient signInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_screen);
+        signIn = findViewById(R.id.signIn);
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        signInClient = GoogleSignIn.getClient(this, gso);
 
+        GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
+        if(signInAccount != null) {
+            Toast.makeText(this, "User is already logged in", Toast.LENGTH_SHORT).show();
+        }
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sign = signInClient.getSignInIntent();
+                startActivityForResult(sign, GOOGLE_SIGN_IN_CODE);
+            }
+        });
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GOOGLE_SIGN_IN_CODE) {
+            Task<GoogleSignInAccount> signInTask = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount signInAcc = signInTask.getResult(ApiException.class);
+            } catch (ApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     private Runnable runnable = new Runnable() {
         @Override
         public void run() {
