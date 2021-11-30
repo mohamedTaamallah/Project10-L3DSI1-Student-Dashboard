1078899be7a236d1180ddd09381d8863cc155fb9
mohmamidoss12 <81252054+mohmamidoss12@users.noreply.github.com>
2021-11-30 14:05:57 +0100

    upload image from gallery sql database upload from camera

--- app/src/main/java/com/example/project/AboutFragment.java
+++ app/src/main/java/com/example/project/AboutFragment.java
@@ -1,12 +1,15 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.project.Activites.Upload_image_Activity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,8 +27,9 @@ public class AboutFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public AboutFragment() {
+    public AboutFragment(String matiere_id) {
         // Required empty public constructor
+        mParam1=matiere_id;
     }
 
     /**
@@ -33,15 +37,13 @@ public class AboutFragment extends Fragment {
      * this fragment using the provided parameters.
      *
      * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment AboutFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static AboutFragment newInstance(String param1, String param2) {
-        AboutFragment fragment = new AboutFragment();
+    public static AboutFragment newInstance(String param1) {
+        AboutFragment fragment = new AboutFragment(param1);
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,6 +51,7 @@ public class AboutFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -58,7 +61,24 @@ public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        View v = inflater.inflate(R.layout.fragment_about, container, false);
+        FloatingActionButton addPhoto = v.findViewById(R.id.AddPhoto);
+        //Navigate to the add photo
+        addPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =new Intent(getActivity(), Upload_image_Activity.class);
+                i.putExtra("matiere",mParam1);
+                startActivity(i);
+
+
+            }
+        });
+        return v;
+
     }
+
+
 }
--- app/src/main/java/com/example/project/Activites/Upload_image_Activity.java
+++ app/src/main/java/com/example/project/Activites/Upload_image_Activity.java
@@ -1,14 +1,13 @@
 package com.example.project.Activites;
 
-import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
 import android.provider.MediaStore;
 import android.view.View;
-import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -16,28 +15,26 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.project.Adapters.MyContextApp;
-import com.example.project.Model.Image;
 import com.example.project.R;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.example.project.SQL_lite.DataBaseHandler;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.StorageTask;
-import com.google.firebase.storage.UploadTask;
 
-import java.io.IOException;
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class Upload_image_Activity extends AppCompatActivity {
 
 
     private static final int PICK_IMAGE_REQUEST = 1;
+    private static final int REQUEST_IMAGE_CAPTURE=101;
 
     private Button mButtonChooseImage;
     private Button mButtonUpload;
@@ -45,7 +42,7 @@ public class Upload_image_Activity extends AppCompatActivity {
     private EditText mEditTextFileName;
     private ImageView mImageView;
     private ProgressBar mProgressBar;
-
+    private DataBaseHandler db ;
     private Uri mImageUri;
 
     private StorageReference mStorageRef;
@@ -53,12 +50,14 @@ public class Upload_image_Activity extends AppCompatActivity {
 
     private StorageTask mUploadTask;
     MyContextApp appContext;
+    private int photo_id= 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.upload_image);
         setTitle("Upload image");
+        db =new DataBaseHandler(this);
 
         mButtonChooseImage = findViewById(R.id.button_choose_image);
         mButtonUpload = findViewById(R.id.button_upload);
@@ -92,7 +91,7 @@ public class Upload_image_Activity extends AppCompatActivity {
         mTextViewShowUploads.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                takePicture();
             }
         });
     }
@@ -103,6 +102,15 @@ public class Upload_image_Activity extends AppCompatActivity {
         intent.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(intent, PICK_IMAGE_REQUEST);
     }
+    private void takePicture(){
+        Intent imageTakeIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if(imageTakeIntent.resolveActivity(getPackageManager())!=null)
+        {
+            startActivityForResult(imageTakeIntent,REQUEST_IMAGE_CAPTURE);
+
+        }
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -111,61 +119,55 @@ public class Upload_image_Activity extends AppCompatActivity {
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
                 && data != null && data.getData() != null) {
             mImageUri = data.getData();
-
+            InputStream inputStream = null;
             try {
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),mImageUri);
-                mImageView.setImageBitmap(bitmap);
-            } catch (IOException e) {
+                inputStream = getContentResolver().openInputStream(mImageUri);
+            } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
+
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                mImageView.setImageBitmap(bitmap);
+        }
+        else if(requestCode==REQUEST_IMAGE_CAPTURE&&resultCode==RESULT_OK )
+        {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+
         }
     }
 
-    private String getFileExtension(Uri uri) {
-        ContentResolver cR = getContentResolver();
-        MimeTypeMap mime = MimeTypeMap.getSingleton();
-        return mime.getExtensionFromMimeType(cR.getType(uri));
-    }
+
+
+
 
     private void uploadFile() {
-        if (mImageUri != null) {
-            StorageReference fileReference = mStorageRef.child(System.currentTimeMillis()
-                    + getFileExtension(mImageUri) );
-//le source de probleme
-            mUploadTask = fileReference.putFile(mImageUri)
-                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                            Handler handler = new Handler();
-                            handler.postDelayed(new Runnable() {
-                                @Override
-                                public void run() {
-                                    mProgressBar.setProgress(0);
-                                }
-                            }, 500);
+        if (mImageView != null) {
+            Intent intent=getIntent();
+            String matiere_id = intent.getStringExtra("matiere");
+            Toast.makeText(Upload_image_Activity.this, matiere_id+"", Toast.LENGTH_SHORT).show();
 
-                            Toast.makeText(Upload_image_Activity.this, "Upload successful", Toast.LENGTH_LONG).show();
-                            Image upload = new Image(mEditTextFileName.getText().toString().trim(),
-                                    taskSnapshot.getMetadata().getReference().getDownloadUrl().toString());
-                            String uploadId = mDatabaseRef.push().getKey();
-                            mDatabaseRef.child("images/").setValue(upload);
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Toast.makeText(Upload_image_Activity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                    })
-                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
-                            double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
-                            mProgressBar.setProgress((int) progress);
-                        }
-                    });
-        } else {
-            Toast.makeText(this, "No file selected", Toast.LENGTH_SHORT).show();
+            if(db.insertImage(String.valueOf(photo_id+1),mEditTextFileName.getText().toString(),imageViewToByte(mImageView),matiere_id))
+            {
+                Toast.makeText(Upload_image_Activity.this, "image enregistrée ", Toast.LENGTH_SHORT).show();
+
+            }
+            else{
+                Toast.makeText(Upload_image_Activity.this, "echec de enregistrement ", Toast.LENGTH_SHORT).show();
+
+            }
         }
+        else{
+
+            Toast.makeText(Upload_image_Activity.this, "Entrer votre image  ", Toast.LENGTH_SHORT).show();
+        }
+    }
+    public static byte[] imageViewToByte(ImageView image) {
+        Bitmap bitmap = ((BitmapDrawable)image.getDrawable()).getBitmap();
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
+        return byteArray;
     }
 }
